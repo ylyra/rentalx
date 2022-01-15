cbcd24fcc8896f5931772b6890a9bace2000435a
Yan Lyra <yanlyra3@gmail.com>
2022-01-15 18:59:51 -0300

    🥅 Added error handler to project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import cors from "cors";
 import express from "express";
+import "express-async-errors";
+import helmet from "helmet";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import errorHandler from "./errors/handler";
 import { router } from "./routes/index.routes";
 import swaggerSetup from "./swagger.json";
 
@@ -11,7 +15,12 @@ const app = express();
 const port = 3333 || process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 app.use("/api/v1", router);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
