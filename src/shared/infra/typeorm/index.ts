813319fc4c79ee487fd6caadd0c5ad167f28ce26
Yan Lyra <yanlyra3@gmail.com>
2022-03-06 01:29:14 -0300

    🚀 Using localhost host on default pg

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 // host = "database_rentalx"
-export default async (host = "database_rentalx"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? process.env.TYPEORM_DATABASE_TEST
